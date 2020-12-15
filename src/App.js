bcbc73580d4929d641aecce7555542a80b7bd200
Maha Mahmood <>
2020-12-14 21:38:21 -0500

    created lottery component to manage state as parent and passed down props. styled lotter component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Ball from './Ball';
+import Lottery from './Lottery';
 
 function App(props) {
   return (
     <div className="App">
       <h1>Lottery App</h1>
-      <Ball num={17}/>
-      <Ball num={13}/>
-      <Ball num={11}/>
-      <Ball num={9}/>
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
     </div>
   );
 }
